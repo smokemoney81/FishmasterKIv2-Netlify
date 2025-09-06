fab19a868adc0c5df4a2c24a8e8b30a24aabb5db
smokemoney81 <kaisaschnitt99@gmail.com>
2025-09-06 02:35:32 +0000

    update: index.ts + .replit

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,12 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  const port = Number(process.env.PORT) || 5000;
+
+server.listen({
+  port,
+  host: "0.0.0.0",
+}, () => {
+  console.log(`✅ Server läuft auf Port ${port}`);
+});
 })();
